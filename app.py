3993a56c6733b075af5406f055bf81c54bee784a
brando1998 <brandodiazmont@gmail.com>
2025-11-11 11:37:32 -0500

    Añadir sistema de notificaciones

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from tkinter import Tk
 from _gui.main_window import AppGUI
+from _utils.actualizaciones import verificar_al_iniciar, VERSION_ACTUAL
 
 if __name__ == "__main__":
     root = Tk()
+    root.title(f"AutoRNDC - v{VERSION_ACTUAL}")
+    root.after(2000, lambda: verificar_al_iniciar())
     app = AppGUI(root)
     root.mainloop()
